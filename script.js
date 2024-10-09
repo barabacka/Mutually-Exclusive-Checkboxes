9c84d418d511e049ea3cb2944df644fa9fda19bb
Aleks Rusak <for.r2z@gmail.com>
2024-10-09 18:47:43 +0200

    v2.0 - conditions

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-//V1.0: logical function
-previous1 = false;
-previous2 = false;
+//V2.0: conditions
+memory = false;
 
 function toggleCheckboxes() {
     const checkbox1 = document.getElementById ( 'checkbox1' );
     const checkbox2 = document.getElementById ( 'checkbox2' );
 
-    y1 = ( checkbox1.checked && !checkbox2.checked ) || ( !previous1 && checkbox1.checked );
-    y2 = ( checkbox2.checked && !checkbox1.checked ) || (  previous1 && checkbox2.checked );
-    previous1         = y1;
-    checkbox1.checked = y1;
-    previous2         = y2;
-    checkbox2.checked = y2;
+    if ( checkbox1.checked && checkbox2.checked )
+    {
+        checkbox2.checked = memory;
+        checkbox1.checked = !memory;
+    }
+    memory = checkbox1.checked;
 }
